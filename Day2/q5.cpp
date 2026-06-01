b55de7d7d0bff445b7d5c9f4c0d8897e15970452
Tanvi Kumari <tanvikumari027@gmail.com>
2026-06-01 11:25:33 +0530

    Updated q5.cpp

--- Day2/q5.cpp
+++ Day2/q5.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n, count = 0;
+    int n, sum = 0;
     cout << "Enter the number whose digits have to be added--";
     cin >> n;
 
     if (n == 0) 
     {
-        count = 1;
+        sum = 0;
     } 
     else 
     {
         while (n > 0) 
         {
+            sum += n % 10;
             n /= 10;
-            count++;
         }
     }
 
-    cout << "The number of digits in the given number is--" << count;
+    cout << "The sum of digits in the given number is--" << sum;
 
     return 0;
 }
