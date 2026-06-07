7f3ab26b7b82608edd530c2c1d0514daf2e0e701
Tanvi Kumari <tanvikumari027@gmail.com>
2026-06-07 17:33:00 +0530

    Updated q17.cpp

--- Day5/q17.cpp
+++ Day5/q17.cpp
@@ -5,22 +5,25 @@ using namespace std;
 int main()
 {
     int num, sum = 0, remainder;
-    cout << "Enter a number to check if it's a happy number: ";
+    cout << "Enter a number to check if it's a perfect number: ";
     cin >> num;
 
-    while (sum != 1)
+    for (int i = 1; i < num; i++)
     {
-        sum = 0;
-        while (num > 0)
+        if (num % i == 0)
         {
-            remainder = num % 10;
-            sum = sum + pow(remainder, 2);
-            num = num / 10;
+            sum += i;
         }
-        num = sum;
     }
 
-    cout << "The number is a happy number." << endl;
+    if (sum == num)
+    {
+        cout << "The number is a perfect number." << endl;
+    }
+    else
+    {
+        cout << "The number is not a perfect number." << endl;
+    }
+
     return 0;
-}
-        
+}    
